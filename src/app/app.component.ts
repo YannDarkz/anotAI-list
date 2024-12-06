82b74ff9f3c0f1c481a943f8f627d24c8096bf23
yanndarkz <iansantosmcqueen@gmail.com>
2024-12-05 22:05:33 -0300

    shore: app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { RouterOutlet, Router } from '@angular/router';
 import { UserService } from './services/user/user.service';
+import { UserFireService } from './services/firebase/user-fire.service';
 
 // import { CommonModule } from '@angular/common';
 // import { AuthButtonsComponent } from './components/auth-buttons/auth-buttons.component';
@@ -16,7 +17,7 @@ import { UserDataService } from './services/user-data/user-data.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public auth: AuthService, private router: Router, private userService: UserService, private userDataService: UserDataService) { }
+  constructor(public auth: AuthService, private userFireService: UserFireService, private router: Router, private userService: UserService, private userDataService: UserDataService) { }
 
 
   showError(message: string) {
@@ -29,8 +30,6 @@ export class AppComponent implements OnInit {
         this.router.navigate(['/listItems']);
 
         this.auth.user$.subscribe(user => {
-          // console.log("uzer", user)
-  
           if (user) {
             const userData = {
               userId: user.sub ? user.sub.split('|')[1] : '',
@@ -38,16 +37,48 @@ export class AppComponent implements OnInit {
               email: user.email || '',
               picture: user.picture || ''
             };
-            this.userService.saveAutorizedUser(userData).subscribe({
-              next: (savedUser) => {
-                if (savedUser) {
-                  this.userDataService.setUserData(savedUser);
-                }
-              }
-            });
+
+            // Salva os dados do usuário no Firestore
+            this.userFireService.saveUser(userData)
+              .then(() => {
+                console.log('Usuário salvo com sucesso no Firestore.');
+                this.userDataService.setUserData(userData); // Atualiza o serviço com os dados do usuário
+              })
+              .catch((error) => {
+                console.error('Erro ao salvar o usuário no Firestore:', error);
+                this.showError('Falha ao salvar os dados do usuário.');
+              });
           }
-        })
+        });
       }
     });
   }
+
+  // ngOnInit() {
+  //   this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+  //     if (isAuthenticated) {
+  //       this.router.navigate(['/listItems']);
+
+  //       this.auth.user$.subscribe(user => {
+  //         // console.log("uzer", user)
+  
+  //         if (user) {
+  //           const userData = {
+  //             userId: user.sub ? user.sub.split('|')[1] : '',
+  //             name: user.name || 'Usuário sem nome',
+  //             email: user.email || '',
+  //             picture: user.picture || ''
+  //           };
+  //           this.userService.saveAutorizedUser(userData).subscribe({
+  //             next: (savedUser) => {
+  //               if (savedUser) {
+  //                 this.userDataService.setUserData(savedUser);
+  //               }
+  //             }
+  //           });
+  //         }
+  //       })
+  //     }
+  //   });
+  // }
 }
