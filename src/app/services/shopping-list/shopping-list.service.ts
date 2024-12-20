9b7d084b0f2836be68471a5b2295f6febf753103
yanndarkz <iansantosmcqueen@gmail.com>
2024-12-20 17:30:12 -0300

    feat: shopping-list service

--- src/app/services/shopping-list/shopping-list.service.ts
+++ src/app/services/shopping-list/shopping-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+
 import { Iproduct } from '../../interfaces/item-list';
-import { Firestore, doc, collection, setDoc, updateDoc, getDoc, arrayUnion, arrayRemove } from '@angular/fire/firestore';
+import { Firestore, doc, collection, query, where, setDoc, updateDoc, getDoc, getDocs, arrayUnion, arrayRemove } from '@angular/fire/firestore';
 
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -11,7 +12,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class ShoppingListService {
 
-  constructor(private http: HttpClient, private firestore: Firestore) { }
+  constructor( private firestore: Firestore) { }
   
   private apiUrl = 'http://localhost:3000';
 
@@ -59,6 +60,33 @@ export class ShoppingListService {
     }
   }
 
+  async addItemBuy(userId: string, category: string, item: Iproduct): Promise<void> {
+    const userRef = doc(this.firestore, `users/${userId}`);
+    const userDoc = await getDoc(userRef);
+  
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      const updatedPurchasedItems = userData['purchasedItems']?.map((list: any) => {
+        // Verifica se a categoria corresponde
+        if (list.category === category) {
+          console.log('Adicionando item na categoria:', category);
+          return {
+            ...list,
+            products: [...list.products, item], // Adiciona o novo item
+          };
+        }
+        return list; // Retorna a lista original para outras categorias
+      });
+  
+      // Atualiza o documento no Firebase
+      await updateDoc(userRef, {
+        purchasedItems: updatedPurchasedItems,
+      });
+    } else {
+      throw new Error('Usuário não encontrado no Firebase.');
+    }
+  }
+
   async updateItem(userId: string, category: string, itemId: string, updatedItem: Iproduct): Promise<void> {
     const userRef = doc(this.firestore, `users/${userId}`);
     const userDoc = await getDoc(userRef);
@@ -81,7 +109,6 @@ export class ShoppingListService {
     }
   }
 
-  // Remove um item de uma categoria
   async deleteItem(userId: string, category: string, itemId: string): Promise<void> {
     const userRef = doc(this.firestore, `users/${userId}`);
     const userDoc = await getDoc(userRef);
@@ -91,6 +118,8 @@ export class ShoppingListService {
       const updatedShoppingLists = userData['shoppingLists']?.map((list: any) => {
         if (list.category === category) {
           const updatedProducts = list.products.filter((product: any) => product.id !== itemId);
+          console.log("sera",updatedProducts);
+          
           return { ...list, products: updatedProducts };
         }
         return list;
@@ -114,6 +143,23 @@ export class ShoppingListService {
       throw new Error('Usuário não encontrado.');
     }
   }
+
+  async getPurchasedItems(category: string): Promise<Iproduct[]> {
+    const collectionRef = collection(this.firestore, 'purchasedItems');
+    const q = query(collectionRef, where('category', '==', category));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })) as Iproduct[];
+  }
+
+  async removePurchasedItem() {
+    console.log("remover item comprado");
+  }
+
+  async addBackToShoppingList() {
+    console.log("adicionar item de volta a lista de compras");
+  }
+
+
 
  
 }
