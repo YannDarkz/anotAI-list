3eb87ef85ca29eb96f020828265aca08bf1379fe
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-03 16:12:07 -0300

    feat: buyItem component

--- src/app/components/buy-item/buy-item.component.ts
+++ src/app/components/buy-item/buy-item.component.ts
@@ -8,6 +8,7 @@ import { Iproduct } from '../../interfaces/item-list';
 import { UserDataService } from '../../services/user-data/user-data.service';
 
 import { filter } from 'rxjs/operators';
+import { Icategory } from '../../services/firebase/user-fire.service';
 
 @Component({
   selector: 'app-buy-item',
@@ -18,17 +19,19 @@ import { filter } from 'rxjs/operators';
   providers: [ShoppingListService]
 })
 export class BuyItemComponent {
-
+  
   userId: string | undefined = undefined;
+  @Output() itemUpdated = new EventEmitter<void>()
 
   constructor(private shoppingService: ShoppingListService, private userData: UserDataService, private itemUpdateService: ItemUpdateService) { }
 
-  purchasedItems: { category: string; products: Iproduct[] }[] = [
-    { category: 'cold', products: [] },
-    { category: 'perishables', products: [] },
-    { category: 'cleaning', products: [] },
-    { category: 'others', products: [] },
-  ];
+  categoriesWithItems: Icategory = {
+    cold: [],
+    perishables: [],
+    cleaning: [],
+    others: []
+
+  };
 
   openCategory: string | null = null;
 
@@ -38,9 +41,10 @@ export class BuyItemComponent {
   @Output() removeItemBuy = new EventEmitter<void>()
 
   async ngOnInit(): Promise<void> {
-    this.userData.getUserData().pipe(
-      filter(data => data !== null) // Ignora valores `null`
-    )
+    this.userData
+      .getUserData()
+      .pipe(filter(data => data !== null) // Ignora valores `null`
+      )
       .subscribe(data => {
         if (data?.userId) {
           this.userId = data.userId;
@@ -52,17 +56,15 @@ export class BuyItemComponent {
   }
 
   async loadPurchasedItems(): Promise<void> {
-    for (const categoryObj of this.purchasedItems) {
+    for (const category of Object.keys(this.categoriesWithItems) as (keyof Icategory)[]) {
       try {
-        const items = await this.shoppingService.getPurchasedItems(this.userId, categoryObj.category);
-        categoryObj.products = items;
+        const items = await this.shoppingService.getPurchasedItems(this.userId, category);
+        this.categoriesWithItems[category] = items;
         this.calculateTotalPrice();
       } catch (error) {
-        console.error(`Erro ao carregar itens comprados da categoria ${categoryObj.category}:`, error);
+        console.error(`Erro ao carregar itens comprados da categoria ${category}:`, error);
       }
     }
-    console.log('pu', this.purchasedItems);
-
   }
 
   resetPurchasedItems(): void {
@@ -84,101 +86,52 @@ export class BuyItemComponent {
     }
   }
 
-  calculateTotalPric(): void {
-    this.buyPrice = this.purchasedItems.reduce(
-      (total, categoryObj) =>
-        total + categoryObj.products.reduce((acc, item) => acc + Number(item.price) * item.quantity, 0),
-      0
-    );
-
-    console.log(this.buyPrice);
-
-  }
 
   calculateTotalPrice(): void {
-    this.buyPrice = this.purchasedItems
-      .reduce((acc, categoryObj) => {
-        const categoryTotal = (categoryObj.products || []).reduce((sum, item) => {
-          // console.log('Tipo da variável item.price:', typeof item.price, item.price);
-          const price = this.convertFormattedPriceToNumber(item.price);
-          // console.log('Tipo da variável price:', typeof price, price);
-
-          const quantity = item.quantity || 1;
-          return sum + price * quantity;
-        }, 0);
-        return acc + categoryTotal;
+    this.buyPrice = Object.values(this.categoriesWithItems).reduce((acc, products) => {
+      const categoryTotal = products.reduce((sum: number, item: Iproduct) => {
+        // Converte o preço formatado para número, se necessário
+        const price = this.convertFormattedPriceToNumber(item.price);
+        const quantity = item.quantity || 1;
+        return sum + price * quantity;
       }, 0);
+      return acc + categoryTotal;
+    }, 0);
   }
 
   convertFormattedPriceToNumber(formattedPrice: string): number {
     if (!formattedPrice) return 0;
-
-    // console.log("chegando", formattedPrice, typeof formattedPrice);
-
-    // const cleanPrice = formattedPrice.replace(/\./g, '').replace(',', '.');
-    // const cleanPrice = formattedPrice.replace(',', '.');
-    // console.log("meio", cleanPrice, typeof cleanPrice);
     const parsedPrice = Number.parseFloat(formattedPrice);
-    // console.log("fim?",  typeof cleanPrice,  typeof parsedPrice, this.maskCurrency(parsedPrice), "karai", parseFloat(this.maskCurrency(parsedPrice)) );
-
     return isNaN(parsedPrice) ? 0 : parsedPrice;
   }
 
-  // removeFromPurchasedItems(category: string, index: number): void {
-  //   const categoryObj = this.purchasedItems.find(cat => cat.category === category);
-  //   if (!categoryObj) return;
-
-  //   const itemToRemove = categoryObj.products[index];
-
-  //   // Remover o item do array de comprados e do servidor
-  //   this.shoppingService.removePurchasedItem(itemToRemove).subscribe({
-  //     next: () => {
-  //       categoryObj.products.splice(index, 1);
-  //       this.calculateTotalPrice();
-  //       this.itemRemoved.emit();
-  //       this.removeItemBuy.emit();
-
-  //       // Adicionar o item de volta à lista de compras original
-  //       this.shoppingService.addBackToShoppingList(itemToRemove).subscribe({
-  //         next: () => {
-  //          console.log('Item adicionado de volta à lista de compras');
-  //          this.itemUpdateService.triggerUpdateItems();
-  //         },
-
-  //         error: (error) => console.error("Erro ao retornar item para a lista de compras:", error)
-  //       });
-  //     },
-  //     error: (error) => console.error("Erro ao remover item comprado:", error)
-  //   });
-  // }
-
-  removeFromPurchasedItemst(category: string, index: number) {
+  removeFromPurchasedItems(category: keyof Icategory, index: number) { 
     if (!this.userId) {
       console.error('User ID não encontrado.');
       return;
     }
-
-    const categoryObj = this.purchasedItems.find(cat => cat.category === category);
-    if (!categoryObj) {
+  
+    const categoryItems = this.categoriesWithItems[category];
+    if (!categoryItems) {
       console.error(`Categoria ${category} não encontrada.`);
       return;
     }
-
-    const itemToRemove = categoryObj.products[index];
-
+  
+    const itemToRemove = categoryItems[index];
+  
     // Remove da lista de comprados e adiciona de volta à lista de compras
     this.shoppingService
       .removeFromPurchased(this.userId, itemToRemove, category)
       .then(() => this.shoppingService.addToShoppingList(this.userId!, itemToRemove, category))
       .then(() => {
-        categoryObj.products.splice(index, 1); // Remove o item localmente
+        categoryItems.splice(index, 1); // Remove o item localmente
         this.calculateTotalPrice(); // Atualiza o preço total
+        this.itemUpdated.emit();
         console.log('Item movido para a lista de compras com sucesso.');
       })
       .catch(error => {
         console.error('Erro ao mover o item:', error);
       });
-
   }
 
 }
