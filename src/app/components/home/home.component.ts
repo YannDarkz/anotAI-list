7c253cde2ad6114b9cab55bd9d90f23d4952b95b
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-06 20:55:03 -0300

    shore: home.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,11 @@ import { AuthButtonsComponent } from '../auth-buttons/auth-buttons.component';
 
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [AuthButtonsComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [AuthButtonsComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   title = 'Home Component';
