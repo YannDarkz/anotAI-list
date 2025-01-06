730a209646135ab6b70372549f0fcb3603fbf608
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-06 20:54:47 -0300

    shore: headerComponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
