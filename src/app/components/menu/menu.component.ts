cfaf68f3db6914aaf50020e15e68a0a45777e62a
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-06 20:56:13 -0300

    feat: menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from "../profile/profile.component";
+import { AuthButtonsComponent } from "../auth-buttons/auth-buttons.component";
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  imports: [],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+    selector: 'app-menu',
+    standalone: true,
+    imports: [ProfileComponent, AuthButtonsComponent],
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
 
