0ff72d4ab07b79ef9151e99b1b093f29a6817e71
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-06 20:53:48 -0300

    shore: cardList.ts

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -2,11 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-card-list',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.scss'
+    selector: 'app-card-list',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './card-list.component.html',
+    styleUrl: './card-list.component.scss'
 })
 export class CardListComponent {
 
