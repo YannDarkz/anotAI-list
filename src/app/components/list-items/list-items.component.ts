2e1243eca261d7521ac75e89c22dfc9dce76ecdd
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-07 17:48:42 -0300

    shore: listItems component

--- src/app/components/list-items/list-items.component.ts
+++ src/app/components/list-items/list-items.component.ts
@@ -12,39 +12,36 @@ import { UserDataService } from '../../services/user-data/user-data.service';
 import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
 import { ItemUpdateService } from '../../services/itemUpdate/item-update.service';
 
-
 import { Subscription, forkJoin, of, from } from 'rxjs';
 import { UserService } from '../../services/user/user.service';
 
-
-
 @Component({
-    selector: 'app-list-items',
-    standalone: true,
-    imports: [CommonModule, CardListComponent, AddItemsComponent, BuyItemComponent,],
-    templateUrl: './list-items.component.html',
-    styleUrl: './list-items.component.scss'
+  selector: 'app-list-items',
+  standalone: true,
+  imports: [CommonModule, CardListComponent, AddItemsComponent, BuyItemComponent,],
+  templateUrl: './list-items.component.html',
+  styleUrl: './list-items.component.scss'
 })
-export class ListItemsComponent   {
-
-  userData: Iuser | null = null
-  userId: string | undefined = undefined
-  private updateSubscription!: Subscription;
-
-  constructor(private userService: UserService, private http: ShoppingListService, private itemUpdateService: ItemUpdateService, private userDataService: UserDataService) { }
-
-
-  addTextNotify = '';
-  messageError = '';
-
+export class ListItemsComponent {
+  
   categoriesWithItems: Icategory = {
     cold: [],
     perishables: [],
     nonperishables: [],
     cleaning: [],
     others: []
-
+  
   };
+  userData: Iuser | null = null
+  userId: string | undefined = undefined
+  private updateSubscription!: Subscription;
+
+  constructor(private userService: UserService, private http: ShoppingListService, private itemUpdateService: ItemUpdateService, private userDataService: UserDataService) {
+  }
+
+  addTextNotify = '';
+  messageError = '';
+
 
   openCategory: string | null = null;
 
@@ -53,7 +50,7 @@ export class ListItemsComponent   {
   @ViewChild(AddItemsComponent) addItemsComponent!: AddItemsComponent;
   @ViewChild(BuyItemComponent) buyItemComponent!: BuyItemComponent;
 
-  
+
 
   ngOnInit(): void {
     this.userService.error$.subscribe(errorMsg => {
@@ -63,18 +60,18 @@ export class ListItemsComponent   {
     this.updateSubscription = this.itemUpdateService.updateItems$.subscribe(() => {
       this.loadItems();
     });
-    
+
     this.userDataService.getUserData().subscribe(data => {
       this.userData = data;
       // console.log("UserData", data);
-      
+
       this.userId = data?.userId
-      
+
       if (this.userId) {
         setTimeout(() => {
           this.loadItems();
         }, 100);
-        
+
       }
     });
   }
@@ -96,6 +93,8 @@ export class ListItemsComponent   {
   }
 
   loadItems(): void {
+    const categoryOrder = ['cold', 'perishables', 'nonperishables', 'cleaning', 'others'];
+
     const loadPromises = Object.keys(this.categoriesWithItems).map(async (category) => {
       if (this.userId) {
         try {
@@ -106,69 +105,77 @@ export class ListItemsComponent   {
         }
       }
     });
-  
+
     Promise.all(loadPromises).then(() => {
+      // console.log('Before sorting:', this.categoriesWithItems);
+      // this.categoriesWithItems = Object.keys(this.categoriesWithItems)
+      // .sort((a, b) => categoryOrder.indexOf(a) - categoryOrder.indexOf(b))
+      // .reduce((sortedCategories, key) => {
+      //   sortedCategories[key as keyof Icategory] = this.categoriesWithItems[key as keyof Icategory];
+      //   return sortedCategories;
+      // }, {} as Icategory);
+
       this.calculateTotalPrice();
     });
   }
 
-async deleteItem(category: keyof Icategory, itemId: string): Promise<void> {
-  if (this.userId) { 
-    try {
-     
-      await this.http.deleteItem(this.userId, category, itemId);
-      
-      if (this.categoriesWithItems[category]) {
-        this.categoriesWithItems[category] = this.categoriesWithItems[category].filter(item => item.id !== itemId);
-        console.log('existentes', this.categoriesWithItems[category]);
-        
-      } else {
-        console.error(`Categoria ${category} não existe localmente.`);
+  async deleteItem(category: keyof Icategory, itemId: string): Promise<void> {
+    if (this.userId) {
+      try {
+
+        await this.http.deleteItem(this.userId, category, itemId);
+
+        if (this.categoriesWithItems[category]) {
+          this.categoriesWithItems[category] = this.categoriesWithItems[category].filter(item => item.id !== itemId);
+          console.log('existentes', this.categoriesWithItems[category]);
+
+        } else {
+          console.error(`Categoria ${category} não existe localmente.`);
+        }
+
+        // Exibe uma notificação ao usuário
+        this.notifyRemoveItem();
+      } catch (error) {
+        console.error('Erro ao deletar item:', error);
       }
-
-      // Exibe uma notificação ao usuário
-      this.notifyRemoveItem();
-    } catch (error) {
-      console.error('Erro ao deletar item:', error);
+    } else {
+      console.error('User ID não encontrado.');
     }
-  } else {
-    console.error('User ID não encontrado.');
   }
-}
 
-async buyItem(item: Iproduct, category: keyof Icategory): Promise<void> {
-  if (this.userId) {
-    try {
-      // Chama o serviço para mover o item
-      await this.http.addItemBuy(this.userId, category, item);
-      this.itemUpdateService.triggerUpdateItems();
+  async buyItem(item: Iproduct, category: keyof Icategory): Promise<void> {
+    if (this.userId) {
+      try {
+        // Chama o serviço para mover o item
+        await this.http.addItemBuy(this.userId, category, item);
+        this.itemUpdateService.triggerUpdateItems();
 
-      // Recarrega os dados da interface
-      this.loadItems();
-      this.buyItemComponent.loadPurchasedItems();
-      this.notifyAddBuyItem(); // Notifica o usuário
-    } catch (error) {
-      console.error('Erro ao comprar item:', error);
+        // Recarrega os dados da interface
+        this.loadItems();
+        this.buyItemComponent.loadPurchasedItems();
+        this.notifyAddBuyItem(); // Notifica o usuário
+      } catch (error) {
+        console.error('Erro ao comprar item:', error);
+      }
+    } else {
+      console.error('User ID não encontrado.');
     }
-  } else {
-    console.error('User ID não encontrado.');
   }
-}
 
-calculateTotalPrice(): void {
-  this.totalPrice = Object.values(this.categoriesWithItems).reduce((acc, items) => {
-    const categoryTotal = items.reduce((sum: number, item: Iproduct) => {
-      const price = this.convertFormattedPriceToNumber(item.price);
-      const quantity = item.quantity || 1;
-      return sum + price * quantity;
+  calculateTotalPrice(): void {
+    this.totalPrice = Object.values(this.categoriesWithItems).reduce((acc, items) => {
+      const categoryTotal = items.reduce((sum: number, item: Iproduct) => {
+        const price = this.convertFormattedPriceToNumber(item.price);
+        const quantity = item.quantity || 1;
+        return sum + price * quantity;
+      }, 0);
+      return acc + categoryTotal;
     }, 0);
-    return acc + categoryTotal;
-  }, 0);
-}
+  }
 
   convertFormattedPriceToNumber(formattedPrice: string): number {
     if (!formattedPrice) return 0;
-    
+
     const parsedPrice = Number.parseFloat(formattedPrice);
     return isNaN(parsedPrice) ? 0 : parsedPrice;
   }
@@ -205,24 +212,24 @@ calculateTotalPrice(): void {
     switch (category) {
       case 'cold':
         return 'frios';
-        case 'perishables':
-          return 'perecíveis';
-        case  'nonperishables'  :
-          return 'não perecíveis';
+      case 'perishables':
+        return 'perecíveis';
+      case 'nonperishables':
+        return 'não perecíveis';
       case 'cleaning':
         return 'limpeza';
-        case 'others':
-          return 'outros';
-          default:
-            return category
-          }
-        }
-    clearList(): void {
-      localStorage.removeItem('listaCompras');
+      case 'others':
+        return 'outros';
+      default:
+        return category
     }
+  }
+  clearList(): void {
+    localStorage.removeItem('listaCompras');
+  }
 
   scrollToTop(): void {
-    const scrollDuration = 30; 
+    const scrollDuration = 30;
     const startPosition = window.scrollY;
     const startTime = performance.now();
 
