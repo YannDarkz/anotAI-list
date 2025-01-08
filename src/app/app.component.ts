b0d1fb522e3c5c6659360658b909a7e6ddde2f71
yanndarkz <iansantosmcqueen@gmail.com>
2025-01-08 12:10:45 -0300

    shore: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { RouterOutlet, Router } from '@angular/router';
 import { UserService } from './services/user/user.service';
@@ -10,17 +10,35 @@ import { UserDataService } from './services/user-data/user-data.service';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { MenuComponent } from "./components/menu/menu.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, FooterComponent, MenuComponent],
+    imports: [RouterOutlet, FooterComponent, MenuComponent, CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
   constructor(public auth: AuthService, private userFireService: UserFireService, private router: Router, private userService: UserService, private userDataService: UserDataService) { }
+
+  showScrollToTop = false;
+
+  // Monitorar o evento de rolagem
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    this.showScrollToTop = scrollTop / scrollHeight > 0.2; // Exibir se rolou 20%
+    console.log(this.showScrollToTop);
+    
+  }
+
+  // Voltar ao topo
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 
   showError(message: string) {
