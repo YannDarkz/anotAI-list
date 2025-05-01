581a472ef2c03d4c06a704ae1d42d02d1d4f51e7
yanndarkz <iansantosmcqueen@gmail.com>
2025-05-01 19:42:30 -0300

    chore: refactor home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { AuthButtonsComponent } from '../auth-buttons/auth-buttons.component';
-
+import { Router } from '@angular/router';
+import { UserDataService } from '../../services/user-data/user-data.service'; 
+import { CommonModule } from '@angular/common';
 
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [AuthButtonsComponent],
+    imports: [CommonModule, AuthButtonsComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  title = 'Home Component';
+  
+  constructor(
+    private userDataService: UserDataService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.userDataService.getUserId().subscribe(userId => {
+      if (userId) {
+        setTimeout(() => {
+          this.router.navigate(['/listItems']);
+        }, 2000);
+      }
+    });
+  }
+
+  
+
+
 
 }
