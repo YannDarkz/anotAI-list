569f6870ca63c997c98d548fed3cc94badb9bbba
yanndarkz <iansantosmcqueen@gmail.com>
2025-08-07 00:23:54 -0300

    feat: refactor redirect

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthButtonsComponent } from '../auth-buttons/auth-buttons.component';
+import { AuthService } from '@auth0/auth0-angular';
+
 import { Router } from '@angular/router';
 import { UserDataService } from '../../services/user-data/user-data.service'; 
 import { CommonModule } from '@angular/common';
@@ -15,22 +17,19 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent {
   
   constructor(
+    private auth: AuthService,
     private userDataService: UserDataService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.userDataService.getUserId().subscribe(userId => {
-      if (userId) {
+     this.auth.isAuthenticated$.subscribe(isAuthenticated => {
+      if (isAuthenticated) {
         setTimeout(() => {
           this.router.navigate(['/listItems']);
         }, 1000);
       }
     });
   }
-
-  
-
-
 
 }
